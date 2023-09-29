bc096baf54acb070614de357427b530b33ae2048
Waqas Ahmed <waqas4693@gmail.com>
2023-09-29 09:38:57 +0500

    Test

--- php-server/form-submission.php
+++ php-server/form-submission.php
@@ -6,6 +6,7 @@ if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === $allowed_origi
     header('Access-Control-Allow-Origin: ' . $allowed_origin);
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Content-Type');
+    header('Content-Type: application/json');
 
     $servername = "localhost";
     $username = "piwnrbcd_admin";
@@ -18,21 +19,17 @@ if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === $allowed_origi
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $email = $_POST['email'];
-    $contactNo = $_POST['contactNo'];
-    $selectedCourse = $_POST['selectedCourse'];
+    $data = json_decode(file_get_contents("php://input"));
 
-    $checkQuery = "SELECT id FROM registeration_requests WHERE email = '$email' AND selectedCourse = '$selectedCourse'";
+    $checkQuery = "SELECT id FROM registeration_requests WHERE email = '$data->email' AND selectedCourse = '$data->selectedCourse'";
     $result = $conn->query($checkQuery);
 
     if ($result->num_rows > 0) {
         http_response_code(409);
     } else {
         $insertQuery = "INSERT INTO registeration_requests (firstName, lastName, email, contactNo, selectedCourse)
-                        VALUES ('$firstName', '$lastName', '$email', '$contactNo', '$selectedCourse')";
-
+                        VALUES ('$data->firstName', '$data->lastName', '$data->email', '$data->contactNo', '$data->selectedCourse')";
+    
         if ($conn->query($insertQuery) === TRUE) {
             http_response_code(201);
         } else {
