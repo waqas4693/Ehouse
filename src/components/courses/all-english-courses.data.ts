9d59312eb3c067a8ffcb9c247af472a1ab8ae271
Waqas Ahmed <waqas4693@gmail.com>
2023-09-30 22:47:10 +0500

    Pictures Updated

--- src/components/courses/all-english-courses.data.ts
+++ src/components/courses/all-english-courses.data.ts
@@ -3,7 +3,7 @@ import type { DescriptiveCourse } from '@/interfaces/descriptive-course'
 export const data: Array<DescriptiveCourse> = [
   {
     id: 1,
-    cover: '/images/courses/a9e7b27a0c5e986a22416d79e2e9dba9.jpg',
+    cover: '/images/courses/course-1.png',
     title: 'General English Course',
     duration: '11 Months',
     description: 'The General English course in the UK is designed for individuals who want to improve their language skills for various purposes, including academic, professional, and social communication.',
@@ -17,7 +17,7 @@ export const data: Array<DescriptiveCourse> = [
   },
   {
     id: 2,
-    cover: '/images/courses/alvaro-reyes-qWwpHwip31M-unsplash.jpg',
+    cover: '/images/courses/course-2.png',
     title: 'IELTS Preparation Course',
     duration: '11 Months',
     description: 'By taking this program, you will be equipped to take the International English Language Testing System (IELTS) Exam, which can help you achieve a good score that will benefit your future studies.',
@@ -31,7 +31,7 @@ export const data: Array<DescriptiveCourse> = [
   },
   {
     id: 3,
-    cover: '/images/courses/christopher-gower-m_HRfLhgABo-unsplash.jpg',
+    cover: '/images/courses/course-3.png',
     title: 'Business English Course',
     duration: '11 Months',
     description: 'Business English Course is designed to help students develop and enhance their language skills in the context of professional settings. This course focuses on improving English proficiency in',
