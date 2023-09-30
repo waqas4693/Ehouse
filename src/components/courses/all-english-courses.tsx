5e5919912a32ec4aa65f719348a460a9fe0f3e10
Waqas Ahmed <waqas4693@gmail.com>
2023-09-30 19:02:30 +0500

    Error Fix

--- src/components/courses/all-english-courses.tsx
+++ src/components/courses/all-english-courses.tsx
@@ -1,36 +1,39 @@
-import React, { FC, useState, ChangeEvent, FormEvent } from 'react'
-import Slider, { Settings } from 'react-slick'
-import { useTheme, styled } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import IconArrowBack from '@mui/icons-material/ArrowBack'
-import IconArrowForward from '@mui/icons-material/ArrowForward'
-import DescriptiveCourseCardItem from '@/components/course/descriptive-course-card-item'
-import Modal from '@mui/material/Modal'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import Snackbar from '@mui/material/Snackbar'
-import MuiAlert, { AlertColor, AlertProps } from '@mui/material/Alert'
-import axios from 'axios'
-import { data } from './all-english-courses.data'
+import React, { FC, useState, ChangeEvent, FormEvent } from 'react';
+import Slider, { Settings } from 'react-slick';
+import { useTheme, styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import IconArrowBack from '@mui/icons-material/ArrowBack';
+import IconArrowForward from '@mui/icons-material/ArrowForward';
+import DescriptiveCourseCardItem from '@/components/course/descriptive-course-card-item';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import axios from 'axios';
+import { data } from './all-english-courses.data';
 
 interface SliderArrowArrow {
-  onClick?: () => void
-  type: 'next' | 'prev'
-  className?: 'string'
+  onClick?: () => void;
+  type: 'next' | 'prev';
+  className?: 'string';
 }
 
-const SliderArrow: FC<SliderArrowArrow> = props => {
-  const { onClick, type, className } = props
+const SliderArrow: FC<SliderArrowArrow> = (props) => {
+  const { onClick, type, className } = props;
   return (
     <IconButton
       sx={{
         backgroundColor: 'background.paper',
         color: 'primary.main',
-        '&:hover': { backgroundColor: 'primary.main', color: 'primary.contrastText' },
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          color: 'primary.contrastText',
+        },
         bottom: '-28px !important',
         left: 'unset !important',
         right: type === 'prev' ? '60px !important' : '0 !important',
@@ -38,14 +41,14 @@ const SliderArrow: FC<SliderArrowArrow> = props => {
         boxShadow: 1,
       }}
       disableRipple
-      color='inherit'
+      color="inherit"
       onClick={onClick}
       className={className}
     >
       {type === 'next' ? <IconArrowForward sx={{ fontSize: 22 }} /> : <IconArrowBack sx={{ fontSize: 22 }} />}
     </IconButton>
-  )
-}
+  );
+};
 
 const StyledDots = styled('ul')(({ theme }) => ({
   '&.slick-dots': {
@@ -61,29 +64,29 @@ const StyledDots = styled('ul')(({ theme }) => ({
       },
     },
   },
-}))
+}));
 
 const customInputStyle = {
   input: {
     borderRadius: '8px',
     border: 'none',
   },
-}
+};
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
-})
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>((props, ref) => {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const AllEnglishCourses: FC = () => {
-  const { breakpoints } = useTheme()
-  const matchMobileView = useMediaQuery(breakpoints.down('md'))
+  const { breakpoints } = useTheme();
+  const matchMobileView = useMediaQuery(breakpoints.down('md'));
 
-  const [open, setOpen] = React.useState(false)
-  const handleOpen = (): void => setOpen(true)
-  const handleClose = (): void => setOpen(false)
-  const [snackbarOpen, setSnackbarOpen] = useState(false)
-  const [severity, setSeverity] = React.useState<AlertColor | undefined>('success')
-  const [selectedCourse, setSelectedCourse] = useState<string>('')
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = (): void => setOpen(true);
+  const handleClose = (): void => setOpen(false);
+  const [snackbar, setSnackbar] = useState({ open: false, severity: 'success', message: '' });
+
+  const [selectedCourse, setSelectedCourse] = useState<string>('');
 
   const [formData, setFormData] = useState({
     firstName: '',
@@ -91,7 +94,7 @@ const AllEnglishCourses: FC = () => {
     email: '',
     contactNo: '',
     selectedCourse: '',
-  })
+  });
 
   const resetForm = (): void => {
     setFormData({
@@ -104,46 +107,41 @@ const AllEnglishCourses: FC = () => {
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>): void => {
-    const { name, value } = e.target
-    setFormData(prevData => ({ ...prevData, [name]: value }))
-  }
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
   const handleSubmit = async (e: FormEvent): Promise<void> => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      const response = await axios.post('https://www.ai2terminator.com/form-submission.php', formData)
+      const response = await axios.post('https://www.ai2terminator.com/form-submission.php', formData);
 
-      console.log("Response = ", response['data']['message'])
+      const message = response.data.message;
 
-      if (response.status === 200) {
-        handleClose()
-        handleSnackbarOpen('success')
-        resetForm()
-      } else if (response.status === 409) {
-        handleClose()
-        handleSnackbarOpen('error')
-        resetForm()
-      } else if (response.status === 500) {
-        handleClose()
-        handleSnackbarOpen('error')
-        resetForm()
+      if (response.data.status === 'success') {
+        handleClose();
+        handleSnackbarOpen('success', message);
+        resetForm();
+      } else if (response.data.status === 'failure') {
+        handleClose();
+        handleSnackbarOpen('error', message);
+        resetForm();
       }
     } catch (error) {
-      console.error('Error submitting the form:', error)
+      console.error('Error submitting the form:', error);
     }
-  }
+  };
 
-  const handleSnackbarOpen = (newSeverity: 'success' | 'error'): void => {
-    setSeverity(newSeverity);
-    setSnackbarOpen(true);
+  const handleSnackbarOpen = (newSeverity: string, newMessage: string): void => {
+    setSnackbar({ open: true, severity: newSeverity || 'success', message: newMessage });
   };
 
   const handleSelectCourse = (courseName: string): void => {
-    setSelectedCourse(courseName)
-    formData.selectedCourse = courseName
-    handleOpen()
-  }
+    setSelectedCourse(courseName);
+    formData.selectedCourse = courseName;
+    handleOpen();
+  };
 
   const sliderConfig: Settings = {
     infinite: true,
@@ -151,18 +149,18 @@ const AllEnglishCourses: FC = () => {
     speed: 300,
     slidesToShow: matchMobileView ? 1 : 3,
     slidesToScroll: 1,
-    prevArrow: <SliderArrow type='prev' />,
-    nextArrow: <SliderArrow type='next' />,
+    prevArrow: <SliderArrow type="prev" />,
+    nextArrow: <SliderArrow type="next" />,
     dots: true,
-    appendDots: dots => <StyledDots>{dots}</StyledDots>,
+    appendDots: (dots) => <StyledDots>{dots}</StyledDots>,
     customPaging: () => (
       <Box sx={{ height: 8, width: 30, backgroundColor: 'divider', display: 'inline-block', borderRadius: 4 }} />
     ),
-  }
+  };
 
   return (
     <Box
-      id='mentors'
+      id="mentors"
       sx={{
         pt: {
           xs: 6,
@@ -175,12 +173,12 @@ const AllEnglishCourses: FC = () => {
         backgroundColor: '#ecf3f3',
       }}
     >
-      <Container maxWidth='lg'>
-        <Typography variant='h1' sx={{ fontSize: 40, color: 'secondary.main' }}>
+      <Container maxWidth="lg">
+        <Typography variant="h1" sx={{ fontSize: 40, color: 'secondary.main' }}>
           Courses
         </Typography>
         <Slider {...sliderConfig}>
-          {data.map(item => (
+          {data.map((item) => (
             <DescriptiveCourseCardItem
               key={String(item.id)}
               item={item}
@@ -190,8 +188,8 @@ const AllEnglishCourses: FC = () => {
         </Slider>
       </Container>
       <Modal
-        aria-labelledby='register-now-modal-title'
-        aria-describedby='register-now-modal-description'
+        aria-labelledby="register-now-modal-title"
+        aria-describedby="register-now-modal-description"
         open={open}
         onClose={handleClose}
         closeAfterTransition
@@ -209,88 +207,87 @@ const AllEnglishCourses: FC = () => {
             py: 5,
           }}
         >
-          <Typography variant='h2' align='center' color='secondary.main' fontSize='48px'>
+          <Typography variant="h2" align="center" color="secondary.main" fontSize="48px">
             Registration Form!
           </Typography>
-          <Typography align='center' sx={{ mt: 1, fontSize: '20px', color: '#232323' }}>
+          <Typography align="center" sx={{ mt: 1, fontSize: '20px', color: '#232323' }}>
             Enroll yourself to access this course.
           </Typography>
           <form onSubmit={handleSubmit}>
             <Box sx={customInputStyle}>
               <TextField
-                name='firstName'
-                label='First Name'
+                name="firstName"
+                label="First Name"
                 fullWidth
-                variant='filled'
-                size='small'
+                variant="filled"
+                size="small"
                 InputProps={{ disableUnderline: true, style: customInputStyle.input }}
                 sx={{ mt: 1 }}
                 value={formData.firstName}
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
               />
               <TextField
-                name='lastName'
-                label='Last Name'
+                name="lastName"
+                label="Last Name"
                 fullWidth
-                variant='filled'
-                size='small'
+                variant="filled"
+                size="small"
                 InputProps={{ disableUnderline: true, style: customInputStyle.input }}
                 sx={{ mt: 1 }}
                 value={formData.lastName}
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
               />
               <TextField
-                name='email'
-                label='Email'
+                name="email"
+                label="Email"
                 fullWidth
-                variant='filled'
-                size='small'
+                variant="filled"
+                size="small"
                 InputProps={{ disableUnderline: true, style: customInputStyle.input }}
                 sx={{ mt: 1 }}
                 value={formData.email}
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
               />
               <TextField
-                name='contactNo'
-                label='Contact Number'
+                name="contactNo"
+                label="Contact Number"
                 fullWidth
-                variant='filled'
-                size='small'
+                variant="filled"
+                size="small"
                 InputProps={{ disableUnderline: true, style: customInputStyle.input }}
                 sx={{ mt: 1 }}
                 value={formData.contactNo}
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
               />
               <TextField
-                name='selectedCourse'
-                label='Selected Course'
+                name="selectedCourse"
+                label="Selected Course"
                 fullWidth
-                variant='filled'
-                size='small'
+                variant="filled"
+                size="small"
                 InputProps={{ disableUnderline: true, style: customInputStyle.input }}
                 sx={{ mt: 1 }}
                 value={selectedCourse}
               />
             </Box>
-            <Button type='submit' variant='contained' color='secondary' fullWidth sx={{ mt: 3, borderRadius: '8px' }}>
+            <Button type="submit" variant="contained" color="secondary" fullWidth sx={{ mt: 3, borderRadius: '8px' }}>
               Register Now
             </Button>
           </form>
         </Box>
       </Modal>
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={2000}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
-          {severity === 'success'
-            ? 'Success! Your form has been Your admission form has been received. The admission team will respond within 48 hours.'
-            : 'A request with this email and course is already submitted'}
-        </Alert>
+      <Snackbar open={snackbar.open} autoHideDuration={2000} onClose={() => setSnackbar({ ...snackbar, open: false })}>
+        {snackbar.severity === 'success' ?
+          <Alert onClose={() => setSnackbar({ ...snackbar, open: false })} severity='success' sx={{ width: '100%' }}>
+            {snackbar.message}
+          </Alert>
+          : <Alert onClose={() => setSnackbar({ ...snackbar, open: false })} severity='error' sx={{ width: '100%' }}>
+            {snackbar.message}
+          </Alert>
+        }
       </Snackbar>
     </Box>
-  )
-}
+  );
+};
 
-export default AllEnglishCourses
+export default AllEnglishCourses;
