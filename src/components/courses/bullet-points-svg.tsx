12996de21371f032df34e9d8e99684c64be4d8b2
Waqas Ahmed <waqas4693@gmail.com>
2023-10-22 12:06:56 +0500

    Error Fixed

--- src/components/courses/bullet-points-svg.tsx
+++ src/components/courses/bullet-points-svg.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { FC } from 'react'
 
-const RedCircleSVG = () => (
-  <svg style={{ marginRight: '12px' }} xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
-    <circle cx="4" cy="4" r="4" fill="#DD4949" />
-  </svg>
-);
+const RedCircleSVG: FC = () => {
+  return (
+    <svg style={{ marginRight: '12px' }} xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
+      <circle cx="4" cy="4" r="4" fill="#DD4949" />
+    </svg>
+  );
+}
 
 export default RedCircleSVG
