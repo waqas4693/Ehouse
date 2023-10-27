0a34a9a116f6673225161591d99fb2fb5f644899
Waqas Ahmed <waqas4693@gmail.com>
2023-10-27 11:20:55 +0500

    2 Additional pages developed

--- src/components/footer/footer-navigation.tsx
+++ src/components/footer/footer-navigation.tsx
@@ -48,13 +48,31 @@ const NavigationItem: FC<NavigationItemProps> = ({ label, path }) => {
   )
 }
 
+const UsefulLinks: FC<NavigationItemProps> = ({ label, path }) => {
+  return (
+    <Link href={path} passHref>
+      <MuiLink
+        underline="hover"
+        sx={{
+          display: 'block',
+          // mb: 1,
+          color: 'primary.contrastText',
+        }}
+        target="_blank"
+      >
+        {label}
+      </MuiLink>
+    </Link>
+  )
+}
+
 const FooterNavigation: FC = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md={4}>
         <FooterSectionTitle title="Useful Links" />
         {courseMenu.map(({ label, path }, index) => (
-          <NavigationItem key={index + path} label={label} path={path} />
+          <UsefulLinks key={index + path} label={label} path={path} />
         ))}
       </Grid>
       <Grid item xs={12} md={4}>
