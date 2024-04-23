7794e01f3511520a9f6f9a759886dfcdd31551bd
Waqas Ahmed <waqas4693@gmail.com>
2024-04-23 11:12:01 +0500

    Fee Updated

--- src/components/home/course.data.ts
+++ src/components/home/course.data.ts
@@ -26,49 +26,49 @@ export const data: Array<Course> = [
     id: 4,
     cover: '/images/courses/course-4.png',
     title: 'Principles of Customer Service',
-    duration: '11 Months',
-    price: 5500
+    duration: '3 Weeks',
+    price: 3500
   },
   {
     id: 5,
     cover: '/images/courses/course-5.png',
     title: 'Principles of Business Administration',
-    duration: '11 Months',
-    price: 5500
+    duration: '3 Weeks',
+    price: 3500
   },
   {
     id: 6,
     cover: '/images/courses/course-6.png',
     title: 'Principles of Team Leading',
-    duration: '11 Months',
-    price: 5500
+    duration: '3 Weeks',
+    price: 3500
   },
   {
     id: 7,
     cover: '/images/courses/course-4.png',
     title: 'A2 Test',
-    duration: '20 Minutes',
-    price: 200
+    duration: '4 Months',
+    price: 300
   },
   {
     id: 8,
     cover: '/images/courses/course-5.png',
     title: 'Driving Theory Test',
-    duration: '8 Weeks',
-    price: 300
+    duration: '4 Months',
+    price: 400
   },
   {
     id: 9,
     cover: '/images/courses/course-6.png',
     title: 'Life In The Uk Test',
     duration: '6 Weeks',
-    price: 300
+    price: 400
   },
   {
     id: 10,
     cover: '/images/courses/course-6.png',
     title: 'B1 Test',
-    duration: '10 To 20 Minutes',
-    price: 200
+    duration: '4 Months',
+    price: 300
   }
 ]
